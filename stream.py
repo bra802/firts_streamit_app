fba19b0e9ce3fbf198548d21dc48b324ed4fd654
bra802 <120571290+bra802@users.noreply.github.com>
2023-05-17 12:47:16 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,9 +1,9 @@
-
+## jeu de données fruits
 import pandas 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
-
+## menu breakfast
 import streamlit
 
 streamlit.title('My parents health Diner')
