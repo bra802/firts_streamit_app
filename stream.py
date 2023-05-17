0245dcf8d7e7a537d8b4f5305c2800b6a06c30fd
bra802 <120571290+bra802@users.noreply.github.com>
2023-05-17 12:34:02 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My parents health Diner')
 streamlit.text('Breakfast menu')
-streamlit.text('Burger & tenders')
+streamlit.text(' 🥣 Burger & tenders')
 streamlit.text('ice cream and muffins')
