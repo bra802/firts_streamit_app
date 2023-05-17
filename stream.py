a16f4d2d27add03cee0e0e5f23fa927b6b5f4a81
bra802 <120571290+bra802@users.noreply.github.com>
2023-05-17 13:04:18 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,9 +1,3 @@
-## jeu de données fruits
-import pandas 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-my_fruit_list
 
 ## menu breakfast
 import streamlit
