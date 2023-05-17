bd2b0d068cc42ffb114a3fce945a0f63d9cf001a
bra802 <120571290+bra802@users.noreply.github.com>
2023-05-17 13:28:37 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -25,3 +25,5 @@ streamlit.dataframe(my_fruit_list)
 ## filter les données
 fruits_selected = streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+# display the table on the page
+streamlit.dataframe(fruits_to_show)
